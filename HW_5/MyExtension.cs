440a58d5d54dd03b97a57f56cd04d755680f031f
Ernest <ernesto170696@gmail.com>
2021-03-10 21:24:11 +0300

    Add code

---
+++ HW_5/MyExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5
+{
+   static  class MyExtension
+    {
+
+        public static void Print(this string str)
+        {
+            int i = 0;
+            foreach (char ch in str)
+            {
+                i++;
+            }
+            Console.WriteLine($"В строке str {i} символов");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5
+{
+    class Program { 
+    
+        static void Main(string[] args)
+        {
+            string str = "Реализовать метод расширения для поиска количество символов в строке";
+            List<int> _list = new List<int>();
+            _list.AddRange(new int[] { 1, 2, 3, 3, 4, 5 });
+            foreach (int i in _list.Distinct())
+            {
+              Console.WriteLine(i + " - " + _list.Where(x => x == i).Count() + " раз");
+            }
+            Console.ReadKey();
+            str.Print();
+        }
+    }
+}
